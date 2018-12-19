cc69e2f1184c50f09876fdee11476716fe96b7ac
Josiah Smith <jojorules47@gmail.com>
2018-12-19 01:56:00 -0700

    #include <stdio.h> #include <stdlib.h> #include <string.h> #include <time.h> #include <signal.h> #include <unistd.h> #include <math.h> #include "nanomsg/nn.h" #include "nanomsg/pubsub.h" #include "nanomsg/ws.h" #include "getSamps.h" #include "tuner.h" #include "kiss_fft.h"
    static int do_exit = 0;
    
    void sig_handler(int signo){
    	if (signo==SIGINT){
    	 	printf("Received SIGINT\n");
    		do_exit = 1;
    	}
    }
    
    int fillBuffer(char* buf, int max_size, float* data, int numpts) {
        int cnt = 0;
        int idx = 0;
        //idx += sprintf(&buf[idx], "{\"x\":0,\"y\",1}");
        //return idx;
    
        idx += sprintf(&buf[idx], "{\"x\":[");
    
        for (cnt = 0; cnt < numpts-1; cnt++) {
            idx += sprintf(&buf[idx], "%d,", cnt);
            if (idx >= max_size - 100) {
                return idx;
            }
        }
        idx += sprintf(&buf[idx], "%d],\"y\":[", cnt);
        if (idx >= max_size - 100) {
            return idx;
        }
    
        for (cnt = 0; cnt < numpts-1; cnt++) {
            idx += sprintf(&buf[idx], "%f,", data[cnt]);
        }
        idx += sprintf(&buf[idx], "%f]}", data[cnt]);
        if (idx >= max_size - 100) {
            return idx;
        }
    
        return idx;
    }
    
    int plotData(int sock, float* data, int numpts) {
        char buf[10000];
        int sz_d = fillBuffer(buf, 10000, data, numpts);
        if (sz_d >= 10000 - 100) {
            return -1;
        }
        //printf("SERVER: PUBLISHING DATE %s\n", buf);
        int bytes = nn_send(sock, buf, sz_d, 0);
        if (bytes < 0) {
            fprintf(stderr, "Could not send data");
            return -2;
        }
        return 0;
    }
    
    int* genfilter(int N){
    	int cnt;
    	int *ret = (int*)malloc(N*sizeof(int));
    	for(cnt=0;cnt<N;cnt++){
    		if(cnt<50 || cnt>100){
    			*(ret + cnt) = 0;
    		}
    		else{
    			*(ret + cnt) = 1;
    		}
    	}
    	return ret;
    }
    
    int main (int argc, const char *argv[]){
        	int rc;
        	int sock;
        	int N = 256;
        	int cnt;
    	const char* shm_name = "mem";
    	shared_t* shm_memory;
    	tuner_info_t* tuner_info;
    	complex_char* memory;
    	long samp_num;
    	unsigned int pcnt = 0;
    	_Bool freq = 1;
    	int* filt;
    
    	/*  Open the socket anew. */
    	sock = nn_socket (AF_SP, NN_PUB);
    
    	rc = nn_bind (sock, "ws://0.0.0.0:3001");
    	//nn_setsockopt (sock, NN_WS_MSG_TYPE, NN_WS_MSG_TYPE_TEXT, NULL, 0);
    
    	shm_memory = read_shared_memory(shm_name, sizeof(tuner_info_t));
    	tuner_info = shm_memory->memory;
    	memory = tuner_info->buffer;
    	int buffer_size = sizeof(tuner_info->buffer) / sizeof(complex_char);
    	printf("%ld\n",buffer_size);
    	samp_num = tuner_info->sample_num;
    	long next = samp_num + 200000;
    
    	kiss_fft_cfg cfg;
    	kiss_fft_cpx *cx_in = calloc(N,sizeof(kiss_fft_cpx));
    	kiss_fft_cpx *cx_out = calloc(N,sizeof(kiss_fft_cpx));
    
    	cfg = kiss_fft_alloc(N,0,NULL,NULL);
    
    	float *data = calloc(N, sizeof(float));
    	filt = genfilter(N);
    
    	signal(SIGINT,sig_handler);
    	while(!do_exit) {
    		while(samp_num < next){
    			usleep(50000);
    			samp_num = tuner_info->sample_num;
    		}
    		int end_idx = (int) (samp_num % buffer_size);
    		// if start idx is neg, do two memcpy's
    		int start_idx = end_idx - N;
    		for (cnt = 0; cnt < N; cnt++) {
    			int idx = start_idx + cnt;
    			if (idx<0) idx += buffer_size;
    			if(freq==1){
    				cx_in[cnt].r = (float) (memory[idx].real-127.5f);
    				cx_in[cnt].i = (float) (memory[idx].imag-127.5f);
    			}
    			else{
    			data[cnt] = (float) (memory[idx].real-127.5f);
    			}
    		}
    
    	if(freq==1){
    		kiss_fft(cfg,cx_in,cx_out);
    		for(cnt=0;cnt<N;cnt++){
    			float mag = cx_out[cnt].r * cx_out[cnt].r + cx_out[cnt].i * cx_out[cnt].i;
    			//mag = mag * filt[cnt];
    			data[cnt] = mag;
    		}
    	}
    	int val = plotData(sock, data, N);
    	if (val) {
    		fprintf(stderr, "error sending data\n");
    		break;
            }
    	next += 200000;
    	if (pcnt++>100){
    		printf("Still getting samps\n");
    		pcnt = 0;
    	}
    	//printf("test\n");
        }
    	free(cfg);
    	free(cx_in);
    	free(cx_out);
    	free(data);
    	return 0;
    }

--- demod.c
+++ demod.c
@@ -8,6 +8,9 @@
 //#include "kiss_fft.h"
 #include "getSamps.h"
 #include "tuner.h"
+#include "kiss_fft.h"
+
+#define FILTER_TAP_NUM 33
 
 static int do_exit = 0;
 
@@ -19,11 +22,11 @@ void sig_handler(int signo){
 }
 
 int main() { 
-	int cnt, buf_number, block_size; 
+	int cnt, buf_number, block_size, a; 
 	const char* tune_name = "tuner";
 	long samp_num;
-	int N = 65536;
-	int N2 = 32768;
+	//int N = 65536;
+	//int N2 = 32768;
 	//int ifft_N = 1024;
 	float real;
 	float imag;
@@ -44,14 +47,62 @@ int main() {
 	samp_num = now;
 	buf_number = 0;
 
+	static double filter_taps[FILTER_TAP_NUM] = {-0.016253292838097254,
+		-0.05743840964970983,
+		-0.05596439344342629,
+		0.0001530388985161355,
+		0.03393232492900409,
+		-0.006171498578367088,
+		-0.03114962670706923,
+		0.015513336012730857,
+		0.03248162595456588,
+		-0.02916823667891449,
+		-0.034591216714420045,
+		0.05143456613294535,
+		0.03651811191114433,
+		-0.09849574481007074,
+		-0.03776111670143315,
+		0.315807809989598,
+		0.5383404961323581,
+		0.315807809989598,
+		-0.03776111670143315,
+		-0.09849574481007074,
+		0.03651811191114433,
+		0.05143456613294535,
+		-0.034591216714420045,
+		-0.02916823667891449,
+		0.03248162595456588,
+		0.015513336012730857,
+		-0.03114962670706923,
+		-0.006171498578367088,
+		0.03393232492900409,
+		0.0001530388985161355,
+		-0.05596439344342629,
+		-0.05743840964970983,
+		-0.016253292838097254};
+
+	// this is for N/D decimation
+	int N = 2*block_size;
+	int D = 2*block_size;
+	
+	kiss_fft_cfg cfg;
+	kiss_fft_cpx *cx_in = calloc(N,sizeof(kiss_fft_cpx));
+	kiss_fft_cpx *cx_out = calloc(N,sizeof(kiss_fft_cpx));
+	cfg = kiss_fft_alloc(N,0,NULL,NULL);
+	
+	kiss_fft_cfg icfg;
+	kiss_fft_cpx *icx_in = calloc(D,sizeof(kiss_fft_cpx));
+	kiss_fft_cpx *icx_out = calloc(D,sizeof(kiss_fft_cpx));
+	icfg = kiss_fft_alloc(D,1,NULL,NULL);
+
 	signal(SIGINT,sig_handler);
-	//int tuner_index = 0;
-	//float* datar = calloc(block_size,sizeof(complex_float));
-	//float* datai = calloc(block_size,sizeof(complex_float));
-	float* data = calloc(block_size,sizeof(complex_float));
+	float* data_out = calloc(D/2,sizeof(float));
+	float* data = calloc(N,sizeof(float));
+
 	complex_float last;
 	last.real = 0;
 	last.imag = 0;
+	
 	while(!do_exit){
 		while (samp_num < next){
 			samp_num = tuner_s->sample_num;
@@ -66,17 +117,38 @@ int main() {
 		//	printf("Falling behind\n");
 		//}
 		int start_offset = now % (TUNE_MEM_SIZE);
-		for(cnt=0;cnt<block_size;cnt++){
+		memcpy(cx_in, &cx_in[N/2], N/2*sizeof(kiss_fft_cpx));
+		for(cnt=0;cnt<N/2;cnt++){
 			int idx = cnt + start_offset;
 			complex_float data_cpx;
 		        data_cpx.real = tune_out[idx].real * last.real - tune_out[idx].imag * last.imag;
 			data_cpx.imag = tune_out[idx].real * last.imag + tune_out[idx].imag * last.real;
-			data[cnt] = atan2(data_cpx.imag,data_cpx.real);
+			cx_in[cnt + N/2].r = atan2(data_cpx.imag,data_cpx.real);
+			cx_in[cnt + N/2].i = 0;
+			//data[cnt] = atan2(data_cpx.imag,data_cpx.real);
 			last.real = tune_out[idx].real;
 			last.imag = -tune_out[idx].imag;
 		}
+		
+		/*for(cnt=0;cnt<N/2;cnt++){
+			for(a=0;a<FILTER_TAP_NUM;a++){
+				data_out[cnt] += filter_taps[FILTER_TAP_NUM - 1 - a] * data[cnt + a];
+			}
+		}*/
 
-		fwrite(data, sizeof(float), block_size, stdout);
+		kiss_fft(cfg,cx_in,cx_out);
+		memcpy(icx_in, cx_out, N*sizeof(kiss_fft_cpx));
+		//memcpy(&icx_in[10], &cx_out[10], (D/2-10) * sizeof(kiss_fft_cpx));
+		//memcpy(&icx_in[D/2-10], &cx_out[N-D/2-1-10], (D/2-10) * sizeof(kiss_fft_cpx));
+		
+		kiss_fft(icfg, icx_in, icx_out);
+		
+		for(cnt=0;cnt<D/2;cnt++){
+			data_out[cnt] = icx_out[cnt+D/4].r/(float)(D/2);
+			//if(!cnt) printf("Val is %f\n", data_out[cnt]);
+		}
+
+		fwrite(data_out, sizeof(float), D/2, stdout);
 		//fflush(stdout);
 
 		now += block_size;
@@ -85,6 +157,11 @@ int main() {
 	//free(datar);
 	//free(datai);
 	free(data);
+	free(data_out);
+	free(cx_in);
+	free(cx_out);
+	free(icx_in);
+	free(icx_out);
 	//fclose(afile);
 	return 0;
 }
